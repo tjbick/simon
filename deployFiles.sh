6b021355bd96f2e1c96bc4896973dd6483b41863
Trevor <trevjbick@gmail.com>
2023-02-03 09:54:32 -0700

    update(deployFiles.sh)
    Changed so that key/hostname/service don't need to be entered each
    time.

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-while getopts k:h:s: flag
-do
-    case "${flag}" in
-        k) key=${OPTARG};;
-        h) hostname=${OPTARG};;
-        s) service=${OPTARG};;
-    esac
-done
+# while getopts k:h:s: flag
+# do
+#     case "${flag}" in
+#         k) key=${OPTARG};;
+#         h) hostname=${OPTARG};;
+#         s) service=${OPTARG};;
+#     esac
+# done
 
-if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
-    printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
-    exit 1
-fi
+# if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
+#     printf "\nMissing required parameter.\n"
+#     printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+#     exit 1
+# fi
+
+key=~/dev/communotee.pem
+hostname=communotee.click
+service=simon
 
 printf "\n----> Deploying files for $service to $hostname with $key\n"
 
